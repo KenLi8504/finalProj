cc95de254dd0849a75d98e12a6ff473c3286750f
KenLi8504 <kli22@stuy.edu>
2022-01-13 11:43:26 -0500

    First bit of all the cards and card functions

---
+++ cards.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct card{
+  char rank [10];
+  char suit [10];
+  struct card *next;
+};
+
+
+struct card * makeCard(char * rank, char * suit){
+  struct card *newCard = malloc (sizeof(struct card));
+  strncpy(newCard -> rank,rank,10);
+  strncpy(newCard -> suit,suit,10);
+  return newCard;
+}
+
+void printCard(struct card * cardPointer){
+  printf("You have a %s of %s\n",cardPointer -> rank,cardPointer -> suit);
+}
+
+struct card ** makeDeck(){
+  struct card ** deck = calloc(4,sizeof(struct card));
+  char * suits [4] = {"Diamonds","Clubs","Hearts","Spades"};
+  char * ranks [13] = {"Two", "Three", "Four", "Five", "Six", "Seven",
+                      "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+  for (int i = 0; i < 4; i++){
+    for (int j = 0; j < 13; j++){
+      struct card * cards = makeCard(ranks[j],suits[i]);
+      printCard(cards);
+    }
+  }
+  return deck;
+}
+
+void printHand(struct card ** hand){
+
+}
+
+int main(){
+  struct card ** deckInitial = makeDeck();
+  return 0;
+}
